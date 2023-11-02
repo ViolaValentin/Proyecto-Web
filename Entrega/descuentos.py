4edab2e8f9140921f7fe76128a2ed9aeabed46fb
SantinoCremona <santino.cremona03@gmail.com>
2023-11-02 11:42:13 -0300

    actualizo

--- Entrega/descuentos.py
+++ Entrega/descuentos.py
@@ -3,6 +3,6 @@ descuentos = [
     {"idDescuento": 2, "imagen": "https://www.totalmedios.com/img/noticias/2019/07/5d3848bf676c0__838x390.jpg", "nombre": "GARBARINO", "Descuento": "Ahorra un 30% con este descuento", "categoria":"tecnologia"},
     {"idDescuento": 3, "imagen": "https://c.static-nike.com/a/images/w_1920,c_limit/bzl2wmsfh7kgdkufrrjq/image.jpg", "nombre": "NIKE", "Descuento": "Obtén un 10% de descuento", "categoria":"deportes"},
     {"idDescuento": 4, "imagen": "https://staticclvf5a.lavozdelinterior.com.ar/files/promociones/miniatura_270x160_-_hoyts.jpg", "nombre": "CINE HOYTS", "Descuento": "¡Compra ahora y ahorra un 15%", "categoria":"multimedia"},
-    {"idDescuento": 6, "imagen": "https://traviesospetshop.com.ar/wp-content/uploads/2020/10/Purina-proplan-logo-transparente.png", "nombre": "PRO PLAN", "Descuento": "Aprovecha el 20% de descuento", "categoria":"mascotas"},
-    {"idDescuento": 7, "imagen": "https://www.comeros.com.ar/wp-content/uploads/2021/11/jbl-logo-6-1.png", "nombre": "JBL", "Descuento": "Consigue un descuento del 12%", "categoria":"musica"},
+    {"idDescuento": 5, "imagen": "https://traviesospetshop.com.ar/wp-content/uploads/2020/10/Purina-proplan-logo-transparente.png", "nombre": "PRO PLAN", "Descuento": "Aprovecha el 20% de descuento", "categoria":"mascotas"},
+    {"idDescuento": 6, "imagen": "https://www.comeros.com.ar/wp-content/uploads/2021/11/jbl-logo-6-1.png", "nombre": "JBL", "Descuento": "Consigue un descuento del 12%", "categoria":"musica"},
 ]
